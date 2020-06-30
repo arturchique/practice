553772a478e9066bf985491ce954818fc4c77908
Artur <aepremyan3993@gmail.com>
2020-06-30 22:02:54 +0300

    restarted

--- practice/catalog/models.py
+++ practice/catalog/models.py
@@ -1,11 +1,3 @@
 from django.db import models
+import requests
 
-
-class Product(models.Model):
-    name = models.CharField(max_length=60)
-    info = models.CharField(max_length=150)
-    location = models.ForeignKey('Address', on_delete=models.SET_NULL, null=True)
-
-
-class Address(models.Model):
-    name = models.CharField(max_length=60)
